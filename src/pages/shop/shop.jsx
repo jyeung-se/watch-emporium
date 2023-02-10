22e83a6c1e5bcaa161a13b86d691dc78aec5bf47
Jack Yeung <jyeung.se@gmail.com>
2023-02-09 23:57:29 -0500

    making cart and shop pages to support products listed

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PRODUCTS from '../../products'
+
+const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            Watch Emporium
+        </div>
+        <div className='products'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Shop
---
+++ src/products.js
@@ -0,0 +1,28 @@
+import rolexolivegreen from './assets/rolexolivegreen.jpg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Rolex - Day-Date Olive Green Dial 18K Everose Gold',
+        price: 16000,
+        dial: 'Olive Green',
+        caseSize: 40,
+        productImage: rolexolivegreen
+    },
+    {
+        id: 1,
+        productName: 'Rolex - Day-Date Olive Green Dial 18K Everose Gold',
+        price: 16000,
+        dial: 'Olive Green',
+        caseSize: 40,
+        productImage: rolexolivegreen
+    },
+    {
+        id: 1,
+        productName: 'Rolex - Day-Date Olive Green Dial 18K Everose Gold',
+        price: 16000,
+        dial: 'Olive Green',
+        caseSize: 40,
+        productImage: rolexolivegreen
+    },
+]
