f8974de72644aca7d16609d560fb6baabbd15474
Jack Yeung <jyeung.se@gmail.com>
2023-02-13 01:38:24 -0500

    Added logo to Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { GiShoppingCart } from 'react-icons/gi'
+import logo from '../assets/logo.png'
 import './navbar.css'
+import '../App.css'
 
 
 export const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className='links'>
-            <Link to='/'>Shop</Link>
-            <Link to='/cart'>
-                <GiShoppingCart size={32} />
-            </Link>
-        </div>
+    <div className='navbar-container'>
+      <div className='navbar-logo'>
+        <img src={logo} />
+      </div>
+      <div className='navbar'>
+            <div className='links'>
+                <Link to='/'>Shop</Link>
+                <Link to='/cart'>
+                    <GiShoppingCart size={32} />
+                </Link>
+            </div>
+      </div>
     </div>
   )
 }
