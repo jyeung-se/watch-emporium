2813e3ba2dfc5118fc8d170653fbb4ee2bb3c010
Jack Yeung <jyeung.se@gmail.com>
2023-02-12 20:19:36 -0500

    Added products to display for inventory/sale.

--- src/products.js
+++ src/products.js
@@ -1,22 +1,53 @@
-import rolexolivegreen from './assets/rolexolivegreen.jpg'
-import grandseikosnowsheritage from './assets/grandseikosnowsheritage.jpg'
-import omegaaquaterrasaffron from './assets/omegaaquaterrasaffron.jpg'
+import chanelj12 from './assets/chanelj12.png'
+import diorchiffre from './assets/diorchiffre.png'
+import grandseikogmt from './assets/grandseikogmt.png'
+import grandseikosnowsheritage from './assets/grandseikosnowsheritage.png'
+import longinesolive from './assets/longinesolive.png'
+import omegaaquaterrasaffron from './assets/omegaaquaterrasaffron.png'
+import omegaconstrose from './assets/omegaconstrose.png'
+import rolexairkingblue from './assets/rolexairkingblue.png'
+import rolexdaytona from './assets/rolexdaytona.png'
+import rolexolivegreen from './assets/rolexolivegreen.png'
+import tudorpelagos from './assets/tudorpelagos.png'
+import tudorpepsi from './assets/tudorpepsi.png'
+import vacheronblue from './assets/vacheronblue.png'
+import vacheronrosebrown from './assets/vacheronrosebrown.png'
 
 
 export const PRODUCTS = [
     {
         id: 1,
-        productName: 'Rolex - Day-Date 18K Everose Gold',
-        brand: 'Rolex',
-        price: 48000,
-        dial: 'Olive Green',
-        caseSize: 40,
+        productName: 'Chanel - J12 Paradoxe Calibre 12.1',
+        brand: 'Chanel',
+        price: 12500,
+        dial: 'Black',
+        caseSize: 38,
         movement: 'Automatic',
-        productImage: rolexolivegreen
+        productImage: chanelj12
     },
     {
         id: 2,
-        productName: 'Grand Seiko - Heritage Hi-Beat 36000',
+        productName: 'Christian Dior - Chiffre Rouge D01',
+        brand: 'Christian Dior',
+        price: 4400,
+        dial: 'Black',
+        caseSize: 42,
+        movement: 'Automatic',
+        productImage: diorchiffre
+    },
+    {
+        id: 3,
+        productName: 'Grand Seiko - Mechanical Hi-Beat 36000 GMT',
+        brand: 'Grand Seiko',
+        price: 6800,
+        dial: 'Blue',
+        caseSize: 44,
+        movement: 'Automatic',
+        productImage: grandseikogmt
+    },
+    {
+        id: 4,
+        productName: 'Grand Seiko - Snow Heritage Hi-Beat 36000',
         brand: 'Grand Seiko',
         price: 7000,
         dial: 'Light Blue',
@@ -25,13 +56,103 @@ export const PRODUCTS = [
         productImage: grandseikosnowsheritage
     },
     {
-        id: 3,
+        id: 5,
+        productName: 'Longines - Master Collection',
+        brand: 'Longines',
+        price: 2225,
+        dial: 'Olive Green',
+        caseSize: 40,
+        movement: 'Automatic',
+        productImage: longinesolive
+    },
+    {
+        id: 6,
         productName: 'Omega - Aqua Terra Co-Axial',
         brand: 'Omega',
-        price: 11000,
+        price: 6800,
         dial: 'Saffron',
         caseSize: 38,
         movement: 'Automatic',
         productImage: omegaaquaterrasaffron
+    },
+    {
+        id: 7,
+        productName: 'Omega - Constellation 18K Rose Gold',
+        brand: 'Omega',
+        price: 8900,
+        dial: 'Blue',
+        caseSize: 38,
+        movement: 'Automatic',
+        productImage: omegaconstrose
+    },
+    {
+        id: 8,
+        productName: 'Rolex - Air King 14000',
+        brand: 'Rolex',
+        price: 4950,
+        dial: 'Blue',
+        caseSize: 34,
+        movement: 'Automatic',
+        productImage: rolexairkingblue
+    },
+    {
+        id: 9,
+        productName: 'Rolex - Cosmograph Daytona 18K Gold',
+        brand: 'Rolex',
+        price: 19100,
+        dial: 'White',
+        caseSize: 40,
+        movement: 'Automatic',
+        productImage: rolexdaytona
+    },
+    {
+        id: 10,
+        productName: 'Rolex - Day-Date 18K Everose Gold',
+        brand: 'Rolex',
+        price: 41500,
+        dial: 'Olive Green',
+        caseSize: 40,
+        movement: 'Automatic',
+        productImage: rolexolivegreen
+    },
+    {
+        id: 11,
+        productName: 'Tudor - Pelagos 39',
+        brand: 'Tudor',
+        price: 4600,
+        dial: 'Black',
+        caseSize: 39,
+        movement: 'Automatic',
+        productImage: tudorpelagos
+    },
+    {
+        id: 12,
+        productName: 'Tudor - Black Bay GMT Pepsi',
+        brand: 'Tudor',
+        price: 4300,
+        dial: 'Black',
+        caseSize: 41,
+        movement: 'Automatic',
+        productImage: tudorpepsi
+    },
+    {
+        id: 13,
+        productName: 'Vacheron Constantin - Overseas Dual Time',
+        brand: 'Vacheron Constantin',
+        price: 27100,
+        dial: 'Blue',
+        caseSize: 41,
+        movement: 'Automatic',
+        productImage: vacheronblue
+    },
+    {
+        id: 14,
+        productName: 'Vacheron Constantin - Overseas Date',
+        brand: 'Vacheron Constantin',
+        price: 31700,
+        dial: 'Black',
+        caseSize: 42,
+        movement: 'Automatic',
+        productImage: vacheronrosebrown
     },
 ]
